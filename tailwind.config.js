fba6fd79f24b37424fbe56f9439244e0c63e717c
Manish Sindur <manishsindur@gmail.com>
2024-04-16 19:58:02 +0530

    Add custom color palette to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         montserrat: ['"Montserrat", sans - serif'],
         fraunces: ['"Fraunces", serif'],
       },
+      colors: {
+        darkcyan: "#2E8B57",
+        cream: "#F8E4D4",
+        verydarkblue: "#1A202C",
+        darkgreyblue: "#70778E",
+      },
     },
   },
   plugins: [],
